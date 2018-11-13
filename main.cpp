66e32a44cc298e0be78e37f0fb2d43b37c8c291b
Dima Tkachenko <dt.mail.gma@gmail.com>
2018-11-13 20:13:42 +0100

    Very First working version of Calculator

--- main.cpp
+++ main.cpp
@@ -8,13 +8,27 @@ int main()
     int b;
     char s;
     cout << "Input a:";
-    cin >> a;
+        cin >> a;
     cout << "Input b:";
-    cin >> b;
+        cin >> b;
     cout << "Input sign:";
-    cin >> s;
-    if (s == '+')
+        cin >> s;
+    if (s == '+') {
         cout << "a + b =" << a + b;
+    }else if (s == '-'){
+        cout << "a - b =" << a - b;
+    }else if (s == '*'){
+        cout << "a * b =" << a * b;
+    }else if (s == '/'){
+        if (b == 0)
+        {
+            cout << "Devide by Zero is denied!";
+            return -1;
+        }
+        cout << "a / b =" << a / b;
+    }else{
+        cout << "Invalid Sign! Avaliable signs: +, -, *, /";
+    }
     cout << endl;
     return 0;
 }
