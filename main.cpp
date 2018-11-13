71e8457f35b9b768ebed0682423319bbcd455833
Dima Tkachenko <dt.mail.gma@gmail.com>
2018-11-13 10:08:42 +0100

    Input sign added

--- main.cpp
+++ main.cpp
@@ -6,11 +6,15 @@ int main()
 {
     int a;
     int b;
+    char s;
     cout << "Input a:";
     cin >> a;
     cout << "Input b:";
     cin >> b;
-    cout << "a + b = " << a + b;
+    cout << "Input sign:";
+    cin >> s;
+    if (s == '+')
+        cout << "a + b =" << a + b;
     cout << endl;
     return 0;
 }
